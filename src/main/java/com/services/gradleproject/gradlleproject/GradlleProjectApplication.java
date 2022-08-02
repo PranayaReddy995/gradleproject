e37dbcc0f4a9d4bf71ee9b1d45a0e0b2e02ca23d
pranaya <pranayareddy995@gmail.com>
2022-08-02 13:00:19 +0530

    initialize msg

---
+++ src/main/java/com/services/gradleproject/gradlleproject/GradlleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.services.gradleproject.gradlleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlleProjectApplication.class, args);
+	}
+
+}
